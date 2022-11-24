d87d6eab91c2e5fe8949fe074734f8c643c18e2a
roskodaniel <99694994+roskodaniel@users.noreply.github.com>
2022-11-24 16:55:56 +0100

    Update hbinstall.sh

--- hbinstall.sh
+++ hbinstall.sh
@@ -15,7 +15,7 @@ sudo apt remove nodejs
 curl -sSfL https://repo.homebridge.io/KEY.gpg | sudo gpg --dearmor | sudo tee /usr/share/keyrings/homebridge.gpg  > /dev/null
 echo "deb [signed-by=/usr/share/keyrings/homebridge.gpg] https://repo.homebridge.io stable main" | sudo tee /etc/apt/sources.list.d/homebridge.list > /dev/null
 sudo apt-get update
-sudo apt-get install homebridge
+sudo apt-get install homebridge -y
 echo "                          ";
 echo ">>>>  INSTALLING DONE <<<<";
 echo "  ____   ___  _   _ _____ ";
@@ -28,7 +28,8 @@ echo ">>>>  INSTALLING DONE <<<<";
 echo "                          ";
 echo "                          ";
 echo ">>>>>  YOU CAN NOW REACH HOMEBRIDGE DASHBOARD BY THIS IP:"
+echo " ";
 sudo hostname -I
+echo " ";
 echo ">>>>>  PORT IS: 8581"
-echo "                          ";
-echo "                          ";
+echo " ";
