3e1b06e1020f6ce1da17e5c2b9c07a229825a811
roskodaniel <99694994+roskodaniel@users.noreply.github.com>
2022-11-26 15:52:21 +0100

    Update hbinstall.sh

--- hbinstall.sh
+++ hbinstall.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
-echo "    ___ _   _ ____ _____  _    _     _     ___ _   _  ____   ";
-echo "   |_ _| \ | / ___|_   _|/ \  | |   | |   |_ _| \ | |/ ___|  ";
-echo "    | ||  \| \___ \ | | / _ \ | |   | |    | ||  \| | |  _   ";
-echo "    | || |\  |___) || |/ ___ \| |___| |___ | || |\  | |_| |  ";
-echo "   |___|_| \_|____/ |_/_/   \_\_____|_____|___|_| \_|\____|  ";
+echo " ";
+echo "======================    Welcome to    =====================";
 echo "  _   _  ___  __  __ _____ ____  ____  ___ ____   ____ _____ ";
 echo " | | | |/ _ \|  \/  | ____| __ )|  _ \|_ _|  _ \ / ___| ____|";
 echo " | |_| | | | | |\/| |  _| |  _ \| |_) || || | | | |  _|  _|  ";
 echo " |  _  | |_| | |  | | |___| |_) |  _ < | || |_| | |_| | |___ ";
 echo " |_| |_|\___/|_|  |_|_____|____/|_| \_\___|____/ \____|_____|";
 echo "                                                             ";
-sudo apt remove node
-sudo apt remove nodejs
+echo "=======================   Installer  ========================";
+echo " ";
+echo ">>> Adding Homebridge Repository <<<";
+echo " ";
 curl -sSfL https://repo.homebridge.io/KEY.gpg | sudo gpg --dearmor | sudo tee /usr/share/keyrings/homebridge.gpg  > /dev/null
 echo "deb [signed-by=/usr/share/keyrings/homebridge.gpg] https://repo.homebridge.io stable main" | sudo tee /etc/apt/sources.list.d/homebridge.list > /dev/null
+echo " ";
+echo ">>> Installing Homebridge <<<";
+echo " ";
 sudo apt-get update
-sudo apt-get install homebridge -y
-echo "                          ";
-echo ">>>>  INSTALLING DONE <<<<";
-echo "  ____   ___  _   _ _____ ";
-echo " |  _ \ / _ \| \ | | ____|";
-echo " | | | | | | |  \| |  _|  ";
-echo " | |_| | |_| | |\  | |___ ";
-echo " |____/ \___/|_| \_|_____|";
-echo "                          ";
-echo ">>>>  INSTALLING DONE <<<<";
-echo "                          ";
-echo "                          ";
-echo ">>>>>  YOU CAN NOW REACH HOMEBRIDGE DASHBOARD BY THIS IP:"
+sudo apt-get install homebridge
 echo " ";
-sudo hostname -I
+echo ">>> Homebridge Ready <<<";
 echo " ";
-echo ">>>>>  PORT IS: 8581"
 echo " ";
+echo ">>> You can reach Homebridge UI by going to <<<";
+echo ">>> http://<ip address of your server>:8581 <<<";
+echo "Your Server's ip is:"
+hostname -I
+echo " ";
+echo " ";
+echo "Thank you for using our installer script!"
+echo ">>  github.com/roskodaniel"
+echo ">>  fiverr.com/daniel_rosko"
