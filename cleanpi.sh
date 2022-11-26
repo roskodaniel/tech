fa3b719840badec74748d634f072d7f4f7b5da0c
roskodaniel <99694994+roskodaniel@users.noreply.github.com>
2022-11-26 16:07:35 +0100

    Update cleanpi.sh

--- cleanpi.sh
+++ cleanpi.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
-echo "                                                  ";
-echo ">>>>>>>>>>>>   CLEANING AFTER UPDATE <<<<<<<<<<<<<";
-echo "   ____ _     _____    _    _   _ ___ _   _  ____ ";
-echo "  / ___| |   | ____|  / \  | \ | |_ _| \ | |/ ___|";
-echo " | |   | |   |  _|   / _ \ |  \| || ||  \| | |  _ ";
-echo " | |___| |___| |___ / ___ \| |\  || || |\  | |_| |";
-echo "  \____|_____|_____/_/   \_\_| \_|___|_| \_|\____|";
-echo "                                                  ";
-echo ">>>>>>>>>>>>   CLEANING AFTER UPDATE <<<<<<<<<<<<<";
-echo "                                                  ";
+echo " ";
+echo "==========   CLEANING AFTER UPDATE =========== ";
+echo "   ____ _     _____    _    _   _ _____ ____   ";
+echo "  / ___| |   | ____|  / \  | \ | | ____|  _ \  ";
+echo " | |   | |   |  _|   / _ \ |  \| |  _| | |_) | ";
+echo " | |___| |___| |___ / ___ \| |\  | |___|  _ <  ";
+echo "  \____|_____|_____/_/   \_\_| \_|_____|_| \_\ ";
+echo "                                               ";
+echo "==========   CLEANING AFTER UPDATE  ========== ";
+echo " ";
 sudo apt autoremove
 sudo apt clean
-echo "                          ";
-echo ">>>>>  CLEANING DONE <<<<<";
-echo "  ____   ___  _   _ _____ ";
-echo " |  _ \ / _ \| \ | | ____|";
-echo " | | | | | | |  \| |  _|  ";
-echo " | |_| | |_| | |\  | |___ ";
-echo " |____/ \___/|_| \_|_____|";
-echo "                          ";
-echo ">>>>>  CLEANING DONE <<<<<";
-echo "                          ";
+echo " ";
+echo ">>> Cleaning Done <<<";
+echo " ";
+echo " ";
+echo "Thank you for using our cleaner script!"
+echo ">>  github.com/roskodaniel"
+echo ">>  fiverr.com/daniel_rosko"
